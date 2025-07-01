7bac4de73693c632f166850663051e42fe5aedbd
Ansh <arajsinghjadoun@gmail.com>
2025-07-01 15:20:32 +0530

    Fix: added quotes around dummyRoute.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 // Will use render assigned port or fallback to 5000
 const PORT = process.env.PORT || 5000;
 
-const routeData = JSON.parse(fs.readFileSync(dummyRoute.json, 'utf8'));
+const routeData = JSON.parse(fs.readFileSync('dummyRoute.json', 'utf8'));
 
 let index = 0;
 
