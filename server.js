403deef8749ba901f8ca61b3fc931d7fcfb594dc
Ansh <arajsinghjadoun@gmail.com>
2025-07-01 15:13:24 +0530

    Fix: use dynamic PORT for Render

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
-
-const vehicleData = require('./dummyRoute.json');
-
-let index = 0;
+const fs = require('fs');
 
 app.use(cors());
 
+// Will use render assigned port or fallback to 5000
+const PORT = process.env.PORT || 5000;
+
+const routeData = JSON.parse(fs.readFileSync(dummyRoute.json, 'utf8'));
+
+let index = 0;
+
 app.get('/api/vehicle-location', (req, res) =>{
-    const point = vehicleData[index];
-    index = (index+1) % vehicleData.length;
-    res.json(point);
+    const location = routeData[index];
+    index = (index+1) % routeData.length;
+    res.json(location);
 });
 
 app.listen(PORT, () => {
