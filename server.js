4b14a0a7d4a7e51d926a923fd5d63684ce696e50
Ansh <arajsinghjadoun@gmail.com>
2025-07-01 17:10:31 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const fs = require('fs');
 
-const app = express();
+app.use(cors());
+
+// Will use render assigned port or fallback to 5000
 const PORT = process.env.PORT || 5000;
 
-app.use(cors()); // 🔥 this fixes CORS
-
 const routeData = JSON.parse(fs.readFileSync('dummyRoute.json', 'utf8'));
+
 let index = 0;
 
-app.get('/api/vehicle-location', (req, res) => {
-  const data = routeData[index];
-  res.json(data);
-  index = (index + 1) % routeData.length;
+app.get('/api/vehicle-location', (req, res) =>{
+    const location = routeData[index];
+    index = (index+1) % routeData.length;
+    res.json(location);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
